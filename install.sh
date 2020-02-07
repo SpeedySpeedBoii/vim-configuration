f513ea4cb6476afd42ec2d2862cb5217edab0cd3
Kururugi <yuvalw98@gmail.com>
2020-02-07 23:41:11 +0100

    Install.sh not printing vim errors anymore

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ mkdir ~/.vim/bundle
 mkdir ~/.vim/bundle/Vundle.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ~/.vim/.vimrc ~/.vimrc
-vim +PluginInstall +qall
+vim -s +PluginInstall +qall
