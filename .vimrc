429badb1912266ea7fe1eeab51019c7daf6e2888
Kururugi <yuvalw98@gmail.com>
2020-02-07 23:33:36 +0100

    fixed install.sh and added vimrc file

---
+++ .vimrc
@@ -0,0 +1,4 @@
+source ~/.vim/vundle.vim
+source ~/.vim/plugin_config.vim
+source ~/.vim/maps.vim
+source ~/.vim/config.vim
--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+mkdir ~/.vim/bundle
 mkdir ~/.vim/bundle/Vundle.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
