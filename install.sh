7e25e7d983f38d22971572e0a70a5502f081b5f2
Kururugi <yuvalw98@gmail.com>
2020-02-07 23:38:01 +0100

    More fixes for install.sh and .vimrc

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 mkdir ~/.vim/bundle
 mkdir ~/.vim/bundle/Vundle.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
 cp ~/.vim/.vimrc ~/.vimrc
+vim +PluginInstall +qall
